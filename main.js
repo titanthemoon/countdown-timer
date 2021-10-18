d2539ba829ecc85b6625ca2aeec15cd80c89471f
titanthemoon <72896383+titanthemoon@users.noreply.github.com>
2021-10-18 15:26:19 -0700

    Add files

---
+++ main.js
@@ -0,0 +1,17 @@
+let countdownDate = new Date("Oct 19, 2021 7:45:00").getTime();
+
+let now, days, hours, minutes, seconds;
+
+let timerLoop = setInterval(function() {
+    now = new Date().getTime();
+
+    const timeLeft = countdownDate - now;
+
+    days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("time").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+
+}, 1000);
