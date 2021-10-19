8f3c4908d941b28c6ec182e9f27c21818405ab2d
titanthemoon <72896383+titanthemoon@users.noreply.github.com>
2021-10-18 17:46:09 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,33 @@ let countdownDate = new Date("Oct 19, 2021 7:45:00").getTime();
 
 let now, days, hours, minutes, seconds;
 
+let pastCountdown;
+
 let timerLoop = setInterval(function() {
     now = new Date().getTime();
 
-    const timeLeft = countdownDate - now;
+    if (countdownDate > now || pastCountdown == true) {
+        const timeLeft = countdownDate - now;
 
-    days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    
+        document.getElementById("time").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+    } else {
+        countdownDate = new Date("Oct 20, 2021 7:45:00").getTime();
 
-    document.getElementById("time").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+        const timeLeft = countdownDate - now;
+
+        days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    
+        pastCountdown = true;
+
+        document.getElementById("time").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+    }
 
 }, 1000);
